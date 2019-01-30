76eb6fce5976eed5cebaaef14eee3eb24ae3c27c
Cory <c.donkin@elifesciences.org>
2019-01-30 13:14:54 +0000

    Add basic tests to manuscriptInputSchema to ensure a valid input passes and no id fails

--- server/xpub-server/entities/manuscript/helpers/manuscriptInputValidationSchema.test.js
+++ server/xpub-server/entities/manuscript/helpers/manuscriptInputValidationSchema.test.js
@@ -1,10 +1,25 @@
 const Joi = require('joi')
 const schema = require('./manuscriptInputValidationSchema')
+const validManuscriptInput = require('./validManuscriptInput.test.json')
+
+const createAlteredManuscript = (valuesToChange, valueToDrop) => {
+  const returnObject = { ...validManuscriptInput, ...valuesToChange }
+  if (valueToDrop) {
+    delete returnObject[valueToDrop]
+  }
+  return returnObject
+}
 
 describe('ManuscriptInputValidationSchema', () => {
-  it('checks the files are in the correct state', () => {
-    const result = Joi.validate({}, schema)
-    console.log(JSON.stringify(result, null, 4))
+  it('There are no errors when validating against a valid input object', () => {
+    const result = Joi.validate(validManuscriptInput, schema)
     expect(result.error).toBe(null)
+  })
+
+  it('Error when no id', () => {
+    const inputValue = createAlteredManuscript({}, 'id')
+    const result = Joi.validate(inputValue, schema)
+    expect(result.error).toBeDefined()
+    expect(result.error.name).toEqual('ValidationError')
   })
 })
